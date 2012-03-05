b5f251bdc74b271e7308158721d2e0ce7cb0fc5a
David Stark <david.stark@zarkonnen.com>
2012-03-05 15:57:13 +0100

    Initial commit.

---
+++ src/com/zarkonnen/spacegen/SpaceGen.java
@@ -0,0 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zarkonnen.spacegen;
+
+public class SpaceGen {
+
+	public static void main(String[] args) {
+	}
+}
